b82b698424bf61b106d1329646c9d72e1893ea6d
Sam Bland <sbland.co.uk@gmail.com>
2021-05-06 20:50:19 +0100

    Implement build dir

--- src/pipeline/build.py
+++ src/pipeline/build.py
@@ -1,6 +1,11 @@
 #! ./venv/bin/python
+import sys
+import shutil
+import os
 from warnings import warn
 import subprocess
+
+BUILD_DIR = "_build_dir"
 CC = "gcc"
 F90 = "gfortran"
 PYTHON = "python"
@@ -76,70 +81,97 @@ SUFFIXES = [".f90", ".fpp"]
 SRC_DIR = "./"
 
 
-def run_subprocess(cmd):
+def run_subprocess(cmd, cwd=".", strict=True):
     print("cmd: [" + " ".join(cmd) + "]")
-    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:
+    with subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd) as p:
         p.wait()
         if p.returncode != 0:
             warn(f"{SRC_DIR} - {PYTHON_MODN} failed {p.stderr.read().decode('utf-8')}")
+            if strict:
+                raise Exception("Process Failed")
 
 
 def libsrc_a_files():
+    print("====== Building libsrc files =====")
     f90_0_files()
     cmd = LIBTOOL + ["libsrc.a"] + LIBSRC_OBJECTS
-    run_subprocess(cmd)
+    run_subprocess(cmd, cwd=BUILD_DIR)
     # ${LIBTOOL} $@ $?
 
 
 def f90_fpp_files():
+    print("====== Building fpp files =====")
     for (fin, fout) in zip(LIBSRC_FILES, LIBSRC_FPP_FILES):
-        cmd = FPP + FPP_F90FLAGS + [fin] + ["-o"] + [fout]
+        cmd = FPP + FPP_F90FLAGS + \
+            [f"-J{BUILD_DIR}"] + [fin] + ["-o"] + [fout]
         run_subprocess(cmd)
+        shutil.move(fout, f"{BUILD_DIR}/{fout}")
         # ${FPP} ${FPP_F90FLAGS} $<  -o $@
+    print("====== ================ =====")
 
 
 def f90_0_files():
+    print("====== Building o files =====")
     for f, o in zip(LIBSRC_FILES, LIBSRC_OBJECTS):
-        cmd = [F90] + F90FLAGS + ["-c"] + [f] + ["-o"] + [o]
+        cmd = [F90] + F90FLAGS + \
+            [f"-J{BUILD_DIR}"] + ["-c"] + [f] + ["-o"] + [o]
         run_subprocess(cmd)
+        shutil.move(o, f"{BUILD_DIR}/{o}")
     # ${F90} ${F90FLAGS} -c $< -o $@
+    print("====== ================ =====")
 
 
 def c_o_files():
+    print("====== Building c files =====")
     cmd = [CC] + CFLAGS + ["-c"] + ["-o"] + LIBSRC_OBJECTS
-    run_subprocess(cmd)
+    run_subprocess(cmd, cwd=BUILD_DIR)
     # ${CC} ${CFLAGS} -c $< -o $@
+    print("====== ================ =====")
 
 
 def build_so_files():
+    print("====== Building so files =====")
     libsrc_a_files()
 
     f90_0_files()
     c_o_files()
 
     f90_fpp_files()
-
+    # === Build so file
     cmd = ["f90wrap", "-m", PYTHON_MODN] + \
         LIBSRC_WRAP_FPP_FILES + ["-k", KIND_MAP, "-v"]
     # f90wrap -m ${PYTHON_MODN} ${LIBSRC_WRAP_FPP_FILES} -k ${KIND_MAP} -v
-    run_subprocess(cmd)
+    run_subprocess(cmd, cwd=BUILD_DIR)
 
     out_files = [f"f90wrap_{f}" for f in LIBSRC_WRAP_FILES]
     cmd = ["f2py-f90wrap", f"--fcompiler={FCOMP}", "--build-dir",
-           ".", "-c", "-m", f"_{PYTHON_MODN}", "-L.", "-lsrc"] + out_files
+           ".", "-c", "-m", f"_{PYTHON_MODN}", f"-L.", "-lsrc"] + out_files
     # f2py-f90wrap --fcompiler=gfortran --build-dir . -c -m _pipelinedemo -L. -lsrc f90wrap*.f90
-    run_subprocess(cmd)
+    run_subprocess(cmd, cwd=BUILD_DIR)
 
+    # === Build so pkg file
+    # TODO: Implement building package file
     # f90wrap -m ${PYTHON_MODN}_pkg ${LIBSRC_WRAP_FPP_FILES} -k ${KIND_MAP} -v -P
     # f2py-f90wrap --fcompiler=$(FCOMP) --build-dir . -c -m _${PYTHON_MODN}_pkg -L. -lsrc f90wrap*.f90
+    print("====== ================ =====")
 
 
 def all():
+    args = sys.argv[1:]
+    if len(args) > 0 and args[0] == "clean":
+        return clean()
+
+    os.makedirs(BUILD_DIR, exist_ok=True)
+    shutil.copyfile(KIND_MAP, f"{BUILD_DIR}/{KIND_MAP}")
     build_so_files()
 
 
 def clean():
-    pass
+    print("Cleaning Dir")
+    try:
+        shutil.rmtree(BUILD_DIR)
+    except Exception:
+        pass
     # -rm -f ${LIBSRC_OBJECTS} ${LIBSRC_FPP_FILES} libsrc.a _${PYTHON_MODN}*.so \
     # _${PYTHON_MODN}_pkg.so *.mod *.fpp f90wrap*.f90 f90wrap*.o *.o ${PYTHON_MODN}.py
     # -rm -rf ${PYTHON_MODN}_pkg
